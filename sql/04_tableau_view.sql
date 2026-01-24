71dc66459c63de3dab7d707cae92925a2ec5e190
Ana Vaz <anafilipasvaz@gmail.com>
2026-01-24 14:57:50 -0800

    Update 04_tableau_view.sql

--- sql/04_tableau_view.sql
+++ sql/04_tableau_view.sql
@@ -1,1 +1,20 @@
+create or replace view no_show_dashboard_base as
+select
+  appointment_id,
+  patient_id,
+  appointment_status,
+  case when appointment_status = 'no_show' then 1 else 0 end as is_no_show,
+  age,
+  age_groups,
+  wait_time_days,
+  wait_time_groups,
+  weekday,
+  sms_received,
+  hipertension, 
+  "Diabetes",
+  alcoholism
+from appointments_clean;
+
+select *
+from no_show_dashboard_base;
 
